c7595f036ed2d4b78bec47acc2d4b55afd9538ec
Parth <developers.roundrobin@gmail.com>
2023-04-13 23:01:33 +0530

    Hyphen.dev

--- src/main/java/org/rorb/dev/hyphen/entity/HyphenUser.java
+++ src/main/java/org/rorb/dev/hyphen/entity/HyphenUser.java
@@ -3,8 +3,10 @@
 package org.rorb.dev.hyphen.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class HyphenUser {
 
     @Id
@@ -16,61 +18,5 @@ public class HyphenUser {
     private String deleteId;
     private String time;
     private String message;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public String getChatId() {
-        return chatId;
-    }
-
-    public void setChatId(String chatId) {
-        this.chatId = chatId;
-    }
-
-    public String getSendId() {
-        return sendId;
-    }
-
-    public void setSendId(String sendId) {
-        this.sendId = sendId;
-    }
-
-    public String getDeleteId() {
-        return deleteId;
-    }
-
-    public void setDeleteId(String deleteId) {
-        this.deleteId = deleteId;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 
 }
